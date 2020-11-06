fdd7092d72ae3c2bbbc66b729a08fbb3520a5d2e
cooperz <coopdawgz92@gmail.com>
2020-11-06 11:09:45 -0800

    Square

--- TelloTemplate.py
+++ TelloTemplate.py
@@ -44,9 +44,9 @@ recvThread.start()
 # CREATE FUNCTIONS HERE....
 
 
-print("\nFirst & Last Names")
-print("Program Name: ")
-print("Date: ")
+print("\nCooper Zervic")
+print("Program Name: Drone Flying School")
+print("Date: 11.6.2020")
 print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
 ready = input('\nAre you ready to take flight: ')
@@ -59,10 +59,17 @@ try:
         sendmsg('command', 0)
         sendmsg('takeoff')
 
-        # Review the (SDK) Software Development Kit resource for Drone Commands
-        # Delete these comments before writing your program
+        # Make a square
+        sendmsg('forward 100')
+        sendmsg('ccw 90')
+        sendmsg('forward 100')
+        sendmsg('ccw 90')
+        sendmsg('forward 100')
+        sendmsg('ccw 90')
+        sendmsg('forward 100')
 
         sendmsg('land')
+
 
         print('\nGreat Flight!!!')
 
